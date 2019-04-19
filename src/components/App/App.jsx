2b8e8ad4c27b83a93621e7a90a2bbd2f36625a2d
PatriotCodes <patriot130690@gmail.com>
2019-04-19 14:32:09 +0300

    Styles additions

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import logo from '../../assets/logo.svg';
-import styles from './App.module.css';
+import styles from './App.module.scss';
+import classNames from 'classnames';
 
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
-        <header className={styles.AppHeader}>
-          <img src={logo} className={styles.AppLogo} alt="logo" />
+        <header className={classNames({[styles.header]: true}, {[styles.blackBg]: true})}>
+          <img src={logo} className={styles.logo} alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className={styles.AppLink}
+            className={styles.link}
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
