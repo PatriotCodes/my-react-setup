1bd05ccb64518260f9f827c5eb3d4d4a97b89de0
PatriotCodes <patriot130690@gmail.com>
2019-04-19 14:39:02 +0300

    Added mobx example code

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,26 @@ import logo from '../../assets/logo.svg';
 import styles from './App.module.scss';
 import classNames from 'classnames';
 
+import { observable, computed } from 'mobx'
+import { observer }  from 'mobx-react'
+
+@observer
 class App extends Component {
+
+  @observable counter = 0;
+
+  onIncrement = () => {
+    this.counter++;
+  }
+
+  onDecrement = () => {
+    this.counter--;
+  }
+
+  @computed get counterFormat() {
+    return `${this.counter}$`
+  }
+
   render() {
     return (
       <div className={styles.App}>
@@ -20,6 +39,14 @@ class App extends Component {
           >
             Learn React
           </a>
+          <p>
+            Mobx example:
+          </p>
+          <div className={styles.mobxExample}>
+            {this.counterFormat}
+            <button onClick={this.onIncrement} type="button">Increment</button>
+            <button onClick={this.onDecrement} type="button">Decrement</button>
+          </div>
         </header>
       </div>
     );
